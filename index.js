6762638e184f94fab0c3f4858d2dfbea29d5a524
caiyou <rodickcai@gmail.com>
2016-09-19 12:18:10 +0800

    import koa

--- index.js
+++ index.js
@@ -2,76 +2,25 @@
 * @Author: caiyou
 * @Date:   2016-09-13 12:08:12
 * @Last Modified by:   caiyou
-* @Last Modified time: 2016-09-19 10:49:18
+* @Last Modified time: 2016-09-19 12:16:12
 */
 
 'use strict';
 
 const co = require('co'),
+      app = require('koa')(),
       iconv = require('iconv-lite'),
       request = require('co-request'),
-      colors = require('colors'),
       fs = require('co-fs');
 
-let prodId = 1164570;
-// let prodId = 1333697;
+// let prodId = 1164570;//瓦尔塔
+let prodId = 2520353;
 let commentFile = '';
 
 function onerror(err) {
   console.error(err.stack);
   console.log(err);
 }
-
-co(function* () {
-
-  let start = +new Date();
-  let page = 0;
-
-  yield fs.open('./content.txt', 'w');
-
-  for(page = 0;; page++) {
-    console.log('正在读取SKU: ' + prodId + '第' +(page + 1)+ '页');
-    commentFile += '>>>>>>>>>>>>>>第'+(page + 1)+'页<<<<<<<<<<<<<<' + '\n\n';
-    // console.log(colors.inverse('>>>>>>>>>>>>>>第'+(page + 1)+'页<<<<<<<<<<<<<<'));
-    let res = yield request({
-      uri: 'http://sclub.jd.com/productpage/p-' + prodId + '-s-0-t-3-p-'+page+'.html?callback=',
-      encoding: null
-    });
-    if(res) {
-      let resData = JSON.parse(iconv.decode(res.body, 'GBK'));
-      let comm = resData.comments;
-      if(comm.length > 0) {
-        comm.forEach(co.wrap(function* (el) {
-          var starStr = convertScoreToStars(el.score);
-          let fileContent = '【' + el.nickname + '】【' + el.referenceTime + '】 ' + starStr + '\n' + el.content + '\n\n'
-          commentFile += fileContent;
-          // let content = '【' + el.nickname.cyan + '】【' + el.referenceTime.red + '】 ' + starStr.yellow + '\n' + el.content + '\n';
-          // console.log(content);
-        }));
-      }else {
-        break;
-      }
-    }
-    if(page % 10 === 0) {
-      yield fs.writeFile('content.txt', commentFile, {
-        flag: 'a+'
-      });
-      commentFile = '';
-    }
-  }
-  if(commentFile) {
-    yield fs.writeFile('content.txt', commentFile, {
-      flag: 'a+'
-    });
-    commentFile = '';
-  }
-
-  let end = +new Date();
-
-  console.log('共爬取' + (page + 1) + '页评论数据');
-  console.log('耗时' + (end-start) + 'ms');
-
-}).catch(onerror);
 
 function convertScoreToStars(score) {
   let starArr = [], starStr;
@@ -83,3 +32,70 @@ function convertScoreToStars(score) {
 
   return starStr;
 }
+
+co(function* () {
+
+  app.use(function* (next) {
+    let start = +new Date();
+    yield next;
+    let end = +new Date();
+    console.log('耗时' + (end-start) + 'ms');
+  });
+
+  app.use(function* (next) {
+    let page = 0;
+
+    yield fs.open('./content.txt', 'w');
+
+    for(page = 0;; page++) {
+      console.log('正在读取SKU: ' + prodId + '第' +(page + 1)+ '页');
+      commentFile += '>>>>>>>>>>>>>>第'+(page + 1)+'页<<<<<<<<<<<<<<' + '\n\n';
+      let res = yield request({
+        uri: 'http://sclub.jd.com/productpage/p-' + prodId + '-s-0-t-3-p-'+page+'.html?callback=',
+        encoding: null
+      });
+      if(res) {
+        let resData = JSON.parse(iconv.decode(res.body, 'GBK'));
+        let comm = resData.comments;
+        if(comm.length > 0) {
+          comm.forEach(co.wrap(function* (el) {
+            var starStr = convertScoreToStars(el.score);
+            let fileContent = '【' + el.nickname + '】【' + el.referenceTime + '】 ' + starStr + el.score +'星' + '\n' + el.content + '\n\n'
+            commentFile += fileContent;
+          }));
+        }else {
+          break;
+        }
+      }
+      if(page % 10 === 0) {
+        yield fs.writeFile('content.txt', commentFile, {
+          flag: 'a+'
+        });
+        commentFile = '';
+      }
+    }
+    if(commentFile) {
+      yield fs.writeFile('content.txt', commentFile, {
+        flag: 'a+'
+      });
+      commentFile = '';
+    }
+    yield fs.writeFile('content.txt', '[该页没有数据]', {
+      flag: 'a+'
+    });
+
+    console.log('共爬取' + (page + 1) + '页评论数据');
+    yield next;
+  });
+
+  app.use(function* () {
+    let content = yield fs.readFile('content.txt', 'utf-8');
+    this.body = content;
+  });
+
+
+
+  app.listen(3000);
+  console.log('crawler is listening at PORT 3000...');
+}).catch(onerror);
+
